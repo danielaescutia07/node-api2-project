f4ce0af242bb81c17b8623b639d654a821868f3f
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-01 12:02:37 -0500

    imported post-model in router to create endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,4 +1,14 @@
 // implement your posts router here
 const router = require('express').Router();
 
+const Post = require('./posts-model');
+
+router.get('/', async (req, res) => {
+  
+});
+
+
+
+
+
 module.exports = router;
