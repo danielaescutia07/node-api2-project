db67da7a59710ac70a25493b23e86629f1215412
danielaescutia07 <danielaescutia@yahoo.com>
2022-04-01 12:17:55 -0500

    get and getById endpoints created in router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,7 +4,50 @@ const router = require('express').Router();
 const Post = require('./posts-model');
 
 router.get('/', async (req, res) => {
-  
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: 'The posts information could not be retrieved',
+      error: err.message
+    });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist'
+      });
+    } else {
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The post information could not be retrieved',
+      error: err.message
+    });
+  }
+});
+
+router.post('/', async (req, res) => {
+
+});
+
+router.put('/:id', async (req, res) => {
+
+});
+
+router.delete('/:id', async (req, res) => {
+
+});
+
+router.get(':id/comments', async (req, res) => {
+
 });
 
 
